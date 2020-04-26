c1f024ea8e2cac885efc0a83d24317ae5a12a994
jdk8_666 <yjdqw@126.com>
2020-04-27 01:21:01 +0800

    p面19正则匹配

--- leetcode/editor/cn/P面试题19ZhengZeBiaoDaShiPiPeiLcof.java
+++ leetcode/editor/cn/P面试题19ZhengZeBiaoDaShiPiPeiLcof.java
@@ -86,7 +86,7 @@ class Solution {
             for (int j = 1; j <= columns; j++) {
                 if (s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '.')
                     dp[i][j] = dp[i-1][j-1];
-                else if (p.charAt(j-1) == '*' && j > 2) {
+                else if (p.charAt(j-1) == '*' && j >= 2) {
                         if (p.charAt(j-2) == s.charAt(i-1) || p.charAt(j-2) == '.')
                             dp[i][j] = dp[i-1][j] || dp[i][j-1];
                         dp[i][j] = dp[i][j] || dp[i][j-2];
