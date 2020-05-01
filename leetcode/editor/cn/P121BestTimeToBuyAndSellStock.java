69a7dc234ef285eb30aa19183c833d15159144e1
jdk8_666 <yjdqw@126.com>
2020-05-01 16:31:43 +0800

    P121  当可交易次数为1，空间复杂度优化

--- leetcode/editor/cn/P121BestTimeToBuyAndSellStock.java
+++ leetcode/editor/cn/P121BestTimeToBuyAndSellStock.java
@@ -34,21 +34,35 @@ public class P121BestTimeToBuyAndSellStock{
         System.out.println(solution.maxProfit(new int[]{1,1,1,1}));
     }
     //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int maxProfit(int[] prices) {
-        int len = prices.length;
-        if (len <= 1)
-            return 0;
-        int[][] dp= new int[len][2];
-        dp[0][0] = 0;
-        dp[0][1] = -prices[0];
-        for (int i = 1; i < len; i++) {
-            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
-            dp[i][1] = Math.max(dp[i-1][1], -prices[i]);
+//class Solution {
+//    public int maxProfit(int[] prices) {
+//        int len = prices.length;
+//        if (len <= 1)
+//            return 0;
+//        int[][] dp= new int[len][2];
+//        dp[0][0] = 0;
+//        dp[0][1] = -prices[0];
+//        for (int i = 1; i < len; i++) {
+//            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
+//            dp[i][1] = Math.max(dp[i-1][1], -prices[i]);
+//        }
+//        return dp[len-1][0];
+//    }
+//}
+    class Solution {
+        public int maxProfit(int[] prices) {
+            int len = prices.length;
+            if (len <= 1)
+                return 0;
+            int dp_i_0 = 0;
+            int dp_i_1 = -prices[0];
+            for (int i = 1; i < len; i++) {
+                dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+                dp_i_1 = Math.max(dp_i_1, -prices[i]);
+            }
+            return dp_i_0;
         }
-        return dp[len-1][0];
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
