eeb9bf4d166cf117da7f6a943bd6944bd341768e
jdk8_666 <yjdqw@126.com>
2020-05-01 23:59:56 +0800

    股票买卖增加限制的

---
+++ leetcode/editor/cn/P188BestTimeToBuyAndSellStockIv.java
@@ -0,0 +1,66 @@
+//给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。 
+//
+// 设计一个算法来计算你所能获取的最大利润。你最多可以完成 k 笔交易。 
+//
+// 注意: 你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。 
+//
+// 示例 1: 
+//
+// 输入: [2,4,1], k = 2
+//输出: 2
+//解释: 在第 1 天 (股票价格 = 2) 的时候买入，在第 2 天 (股票价格 = 4) 的时候卖出，这笔交易所能获得利润 = 4-2 = 2 。
+// 
+//
+// 示例 2: 
+//
+// 输入: [3,2,6,5,0,3], k = 2
+//输出: 7
+//解释: 在第 2 天 (股票价格 = 2) 的时候买入，在第 3 天 (股票价格 = 6) 的时候卖出, 这笔交易所能获得利润 = 6-2 = 4 。
+//     随后，在第 5 天 (股票价格 = 0) 的时候买入，在第 6 天 (股票价格 = 3) 的时候卖出, 这笔交易所能获得利润 = 3-0 = 3 
+//。
+// 
+// Related Topics 动态规划
+
+
+package editor.cn;
+//Java：买卖股票的最佳时机 IV
+public class P188BestTimeToBuyAndSellStockIv{
+    public static void main(String[] args) {
+        Solution solution = new P188BestTimeToBuyAndSellStockIv().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int len = prices.length;
+        if (len <= 1)
+            return 0;
+        if (k > len/2)
+            return maxProfit(prices);
+        int [][][] dp = new int[len][k+1][2];
+        for (int i = 0; i < len; i++) {
+            for (int j = k; j >= 1; j--) {
+                if (i == 0) {
+                    dp[i][j][0] = 0;
+                    dp[i][j][1] = -prices[i];
+                    continue;
+                }
+                dp[i][j][0] = Math.max(dp[i-1][j][0], dp[i-1][j][1] + prices[i]);
+                dp[i][j][1] = Math.max(dp[i-1][j][1], dp[i-1][j-1][0] - prices[i]);
+            }
+        }
+        return dp[len-1][k][0];
+    }
+
+    public int maxProfit(int[] prices) {
+        int ret = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i + 1] > prices[i])
+                ret += prices[i + 1] - prices[i];
+        }
+        return ret;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
