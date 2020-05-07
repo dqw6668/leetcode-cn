eec9e7e374138d137b2cd7a7de8921d30cc796f8
jdk8_666 <yjdqw@126.com>
2020-05-08 00:14:16 +0800

    P120

---
+++ leetcode/editor/cn/P120Triangle.java
@@ -0,0 +1,59 @@
+//给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。 
+//
+// 例如，给定三角形： 
+//
+// [
+//     [2],
+//    [3,4],
+//   [6,5,7],
+//  [4,1,8,3]
+//]
+// 
+//
+// 自顶向下的最小路径和为 11（即，2 + 3 + 5 + 1 = 11）。 
+//
+// 说明： 
+//
+// 如果你可以只使用 O(n) 的额外空间（n 为三角形的总行数）来解决这个问题，那么你的算法会很加分。 
+// Related Topics 数组 动态规划
+
+
+package editor.cn;
+
+import java.util.List;
+
+//Java：三角形最小路径和
+public class P120Triangle{
+    public static void main(String[] args) {
+        Solution solution = new P120Triangle().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle == null || triangle.size() == 0)
+            return 0;
+        int row = triangle.size();
+        int column = triangle.get(row-1).size();
+        int[][] dp = new int[row+1][column+1];
+        dp[0][0] = triangle.get(0).get(0);
+        int res = Integer.MAX_VALUE;
+        for (int i = 1; i < row; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0)
+                    dp[i][j] = dp[i-1][j] + triangle.get(i).get(j);
+                else if (j == i)
+                    dp[i][j] = dp[i-1][j-1] + triangle.get(i).get(j);
+                else
+                    dp[i][j] = Math.min(dp[i-1][j-1], dp[i-1][j]) + triangle.get(i).get(j);
+            }
+        }
+        for (int i = 0; i < column; i++) {
+            res = Math.min(res, dp[row-1][i]);
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
