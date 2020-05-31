39e44e9e0593e9deddbc2738243c4ece878ab947
jdk8_666 <yjdqw@126.com>
2020-05-31 17:23:08 +0800

    dp p46 全排列

---
+++ leetcode/editor/cn/backtrack/P46Permutations.java
@@ -0,0 +1,33 @@
+//给定一个 没有重复 数字的序列，返回其所有可能的全排列。 
+//
+// 示例: 
+//
+// 输入: [1,2,3]
+//输出:
+//[
+//  [1,2,3],
+//  [1,3,2],
+//  [2,1,3],
+//  [2,3,1],
+//  [3,1,2],
+//  [3,2,1]
+//] 
+// Related Topics 回溯算法
+
+
+package editor.cn.backtrack;
+//Java：全排列
+public class P46Permutations{
+    public static void main(String[] args) {
+        Solution solution = new P46Permutations().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
