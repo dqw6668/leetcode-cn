a66061b89111a01458ed9ac9341b66b371a5ce69
jdk8_666 <yjdqw@126.com>
2020-05-31 22:23:23 +0800

    dp p46 全排列

--- leetcode/editor/cn/backtrack/P46Permutations.java
+++ leetcode/editor/cn/backtrack/P46Permutations.java
@@ -16,18 +16,60 @@
 
 
 package editor.cn.backtrack;
+
+import com.sun.deploy.util.ArrayUtil;
+
+import java.util.*;
+
 //Java：全排列
-public class P46Permutations{
+//Date 2020年5月31日
+public class P46Permutations {
     public static void main(String[] args) {
         Solution solution = new P46Permutations().new Solution();
         // TO TEST
+        System.out.println(solution.permute(new int[]{1,2,3}));
     }
-    //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public List<List<Integer>> permute(int[] nums) {
 
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<Integer>> permute(int[] nums) {
+            // 特判
+            int len = nums.length;
+            List<List<Integer>> res = new LinkedList<>();
+            if (len == 0) return res;
+            backtrack(nums, res, 0, len);
+            return res;
+        }
+
+        // 回溯框架
+        public void backtrack(int[] nums, List<List<Integer>> ret, int index, int maxIndex) {
+            // if结束条件
+            if (index == maxIndex - 1) {
+                List<Integer> path = new LinkedList<>();
+                for (int n :
+                        nums) {
+                    path.add(n);
+                }
+                ret.add(path);
+                return;
+            }
+            // for选择
+            for (int i = index; i < maxIndex; i++) {
+                // 做选择
+                swap(nums, index, i);
+                // 递归
+                backtrack(nums, ret, index+1, maxIndex);
+                // 撤销，状态重置
+                swap(nums, index, i);
+            }
+        }
+
+        public void swap(int[] arr, int i, int index) {
+            int tmp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = tmp;
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
