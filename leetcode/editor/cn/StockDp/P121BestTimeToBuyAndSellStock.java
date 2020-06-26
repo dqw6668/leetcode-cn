47eabe532a57a61775f8d1361b5f6a0b774b1947
jdk8_666 <yjdqw@126.com>
2020-06-26 19:52:13 +0800

    p121

--- leetcode/editor/cn/StockDp/P121BestTimeToBuyAndSellStock.java
+++ leetcode/editor/cn/StockDp/P121BestTimeToBuyAndSellStock.java
@@ -23,9 +23,10 @@
 // Related Topics 数组 动态规划
 
 
-package editor.cn.dp;
+package editor.cn.StockDp;
+
 //Java：买卖股票的最佳时机
-public class P121BestTimeToBuyAndSellStock{
+public class P121BestTimeToBuyAndSellStock {
     public static void main(String[] args) {
         Solution solution = new P121BestTimeToBuyAndSellStock().new Solution();
         // TO TEST
@@ -33,31 +34,44 @@ public class P121BestTimeToBuyAndSellStock{
         System.out.println(solution.maxProfit(new int[]{7,1,5,3,6,4}));
         System.out.println(solution.maxProfit(new int[]{1,1,1,1}));
     }
+
     //leetcode submit region begin(Prohibit modification and deletion)
-//class Solution {
-//    public int maxProfit(int[] prices) {
-//        int len = prices.length;
-//        if (len <= 1)
-//            return 0;
-//        int[][] dp= new int[len][2];
-//        dp[0][0] = 0;
-//        dp[0][1] = -prices[0];
-//        for (int i = 1; i < len; i++) {
-//            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
-//            dp[i][1] = Math.max(dp[i-1][1], -prices[i]);
-//        }
-//        return dp[len-1][0];
-//    }
-//}
     class Solution {
         public int maxProfit(int[] prices) {
             int len = prices.length;
-            if (len <= 1)
+            // 特判
+            if (len < 2)
                 return 0;
+            // 定义dp[i][j]表示在第i天 1持有 或者 0不持有 股票时的最大利润
+            int[][] dp = new int[len + 1][2];
+            // base状态 第一天
+            dp[1][0] = 0;
+            dp[1][1] = -prices[0];
+            for (int i = 2; i <= len; i++) {
+                // 当天没有股票可能来自于 头一天没持有 或者 头一天持有但卖出了
+                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1]);
+                // 当天持有股票来自于 头一天就持有 或者头一天没有但是买入 由于只能买卖一次，所以之前的利润都为0
+                dp[i][1] = Math.max(dp[i - 1][1], - prices[i - 1]);
+            }
+            return dp[len][0];
+        }
+    }
+
+    // 空间上优化
+    class Solution2 {
+        public int maxProfit(int[] prices) {
+            int len = prices.length;
+            // 特判
+            if (len < 2)
+                return 0;
+            // 表示在第i天没持有股票的最大利润 初始为第一天的base状态
             int dp_i_0 = 0;
+            // 表示在第i天持有股票的最大利润 初始为第一天的base状态
             int dp_i_1 = -prices[0];
             for (int i = 1; i < len; i++) {
+                // 当天没有股票可能来自于 头一天没持有 或者 头一天持有但卖出了
                 dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+                // 当天持有股票来自于 头一天就持有 或者头一天没有但是买入 由于只能买卖一次，所以之前的利润都为0
                 dp_i_1 = Math.max(dp_i_1, -prices[i]);
             }
             return dp_i_0;
