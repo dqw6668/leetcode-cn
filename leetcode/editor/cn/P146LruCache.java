8eac983d0d76a1ce8bd6067f53c422c76331bfab
jdk8_666 <yjdqw@126.com>
2020-08-06 16:47:53 +0800

    p146 LRU

---
+++ leetcode/editor/cn/P146LruCache.java
@@ -0,0 +1,141 @@
+//运用你所掌握的数据结构，设计和实现一个 LRU (最近最少使用) 缓存机制。它应该支持以下操作： 获取数据 get 和 写入数据 put 。 
+//
+// 获取数据 get(key) - 如果关键字 (key) 存在于缓存中，则获取关键字的值（总是正数），否则返回 -1。 
+//写入数据 put(key, value) - 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字/值」。当缓存容量达到上限时，它应该在
+//写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。 
+//
+// 
+//
+// 进阶: 
+//
+// 你是否可以在 O(1) 时间复杂度内完成这两种操作？ 
+//
+// 
+//
+// 示例: 
+//
+// LRUCache cache = new LRUCache( 2 /* 缓存容量 */ );
+//
+//cache.put(1, 1);
+//cache.put(2, 2);
+//cache.get(1);       // 返回  1
+//cache.put(3, 3);    // 该操作会使得关键字 2 作废
+//cache.get(2);       // 返回 -1 (未找到)
+//cache.put(4, 4);    // 该操作会使得关键字 1 作废
+//cache.get(1);       // 返回 -1 (未找到)
+//cache.get(3);       // 返回  3
+//cache.get(4);       // 返回  4
+// 
+// Related Topics 设计 
+// 👍 798 👎 0
+
+
+package editor.cn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//Java：LRU缓存机制
+public class P146LruCache {
+    public static void main(String[] args) {
+        // TO TEST
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class LRUCache {
+        // 手写链表结构，NODE
+        class DLinkedNode {
+            int key;
+            int value;
+            DLinkedNode prev;
+            DLinkedNode next;
+
+            public DLinkedNode() {
+            }
+
+            public DLinkedNode(int _key, int _value) {
+                key = _key;
+                value = _value;
+            }
+        }
+
+        // hash O(1)的时间找到NODE
+        private Map<Integer, DLinkedNode> cache = new HashMap<>();
+        private int size;
+        private int capacity;
+        private DLinkedNode head, tail;
+
+        // 初始化
+        public LRUCache(int capacity) {
+            this.size = 0;
+            this.capacity = capacity;
+            head = new DLinkedNode();
+            tail = new DLinkedNode();
+            head.next = tail;
+            tail.prev = head;
+        }
+
+        public int get(int key) {
+            DLinkedNode node = cache.get(key);
+            // 不存在的key
+            if (node == null)
+                return -1;
+            // 刚使用过，移到最前面
+            moveToHead(node);
+            return node.value;
+        }
+
+        public void put(int key, int value) {
+            DLinkedNode node = cache.get(key);
+            if (null == node) {
+                DLinkedNode newNode = new DLinkedNode(key, value);
+                cache.put(key, newNode);
+                // 加到双向链表头部
+                addToHead(newNode);
+                size++;
+                if (size > capacity) {
+                    // 双向链表中还要存储key的原因，超出容量移除时，要移除map中的项，需要key，只能由链表中node得到key
+                    DLinkedNode tail = removeTail();
+                    // 用到的key
+                    cache.remove(tail.key);
+                    size--;
+                }
+            } else {
+                node.value = value;
+                moveToHead(node);
+            }
+        }
+
+        private void addToHead(DLinkedNode node) {
+            node.prev = head;
+            node.next = head.next;
+            head.next.prev = node;
+            head.next = node;
+        }
+
+        private void moveToHead(DLinkedNode node) {
+            removeNode(node);
+            addToHead(node);
+        }
+
+        private DLinkedNode removeTail() {
+            DLinkedNode res = tail.prev;
+            removeNode(res);
+            return res;
+        }
+
+        private void removeNode(DLinkedNode node) {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+    }
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
