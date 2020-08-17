b8853fdc6f81b6bbd228e7b7898ccfb4093be3d9
jdk8_666 <yjdqw@126.com>
2020-08-17 18:36:02 +0800

    p110

---
+++ leetcode/editor/cn/tree/TreeNode.java
@@ -0,0 +1,11 @@
+package editor.cn.tree;
+
+/**
+ * Created by Five on 2020/8/17 15:53
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
